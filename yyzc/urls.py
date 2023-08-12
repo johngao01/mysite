c9174ecf20b7a8fe8e350bc1894a61b589e9e621
johngao01 <1649868243@qq.com>
2023-08-12 12:30:06 +0800

    初步完成

--- yyzc/urls.py
+++ yyzc/urls.py
@@ -3,6 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.item_list, name="index"),
-    path("show", views.item_show, name="show    "),
+    path("", views.index, name="index"),
 ]
--- yyzc/views.py
+++ yyzc/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from .models import Statistics
 
 
-def item_list(request):
+def index(request):
     items = Statistics.objects.all()
     context = {
         'title': ['月份', '投产验证总变更单', '应用科驳回', '到达应用科次数', '应用科驳回率',
@@ -17,17 +17,3 @@ def item_list(request):
         'reject_times': [data.times for data in items]
     }
     return render(request, "index.html", context)
-
-
-def item_show(request):
-    items = Statistics.objects.all()
-    context = {
-        'title': 'User Traffic History',
-        'legend': [data.month for data in items],
-        'x_axis_data': [data.month for data in items],
-        'total_number': [data.total_num for data in items],
-        'reject_number': [data.reject for data in items],
-        'reject_rating': [data.reject_rating for data in items],
-        'reject_times': [data.times for data in items]
-    }
-    return render(request, "my_show.html", context)
